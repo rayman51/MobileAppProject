4f8aafa5966784539e438b0e7fb9109b3b5005c8
rayman51 <ray.mannionit2@gmail.com>
2018-12-04 17:51:42 +0000

    added touch firing

--- War in Space/Assets/Scripts/Player.cs
+++ War in Space/Assets/Scripts/Player.cs
@@ -72,7 +72,7 @@ public class Player : MonoBehaviour
 
     private void Die()
     {
-        FindObjectOfType <Level>().LoadGameOver();// loads game over 
+        FindObjectOfType<Level>().LoadGameOver();// loads game over 
         Destroy(gameObject);// destroys object 
         AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);// plays explosion sound
     }
@@ -92,6 +92,12 @@ public class Player : MonoBehaviour
         {
             StopCoroutine(firingCoroutine);
         }// if
+         //Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+         //elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+         /*if (Input.touchCount > 0)
+         {
+             firingCoroutine = StartCoroutine(FireContinuously());
+         }*/
     }// Fire()
 
     IEnumerator FireContinuously()
@@ -128,49 +134,10 @@ public class Player : MonoBehaviour
         var newXpos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
         var newYpos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXpos, newYpos);
-        //Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
-        //elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
 
-        //Check if Input has registered more than zero touches
-        if (Input.touchCount > 0)
-        {
-            //Store the first touch detected.
-            Touch myTouch = Input.touches[0];
-
-            //Check if the phase of that touch equals Began
-            if (myTouch.phase == TouchPhase.Began)
-            {
-                //If so, set touchOrigin to the position of that touch
-                touchOrigin = myTouch.position;
-            }
-
-            //If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
-            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
-            {
-                //Set touchEnd to equal the position of this touch
-                Vector2 touchEnd = myTouch.position;
-
-                //Calculate the difference between the beginning and end of the touch on the x axis.
-                float x = touchEnd.x - touchOrigin.x;
-
-                //Calculate the difference between the beginning and end of the touch on the y axis.
-                float y = touchEnd.y - touchOrigin.y;
-
-                //Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
-                touchOrigin.x = -1;
-
-                //Check if the difference along the x axis is greater than the difference along the y axis.
-                if (Mathf.Abs(x) > Mathf.Abs(y))
-                    //If x is greater than zero, set deltaX to 1, otherwise set it to -1
-                    deltaX = x > 0 ? 1 : -1;
-                else
-                    //If y is greater than zero, set deltaY to 1, otherwise set it to -1
-                    deltaY = y > 0 ? 1 : -1;
-            }
-        }
 
       
-      
-    
-}// Move()
+
+
+    }// Move()
 }
